910ef67832ec4328a05e150dec453867d13cab1f
ElizabethCG <elizabeth.caceres@gmail.com>
2019-02-27 17:30:11 -0300

    crea array con rutas del contenido de una carpeta

--- index.js
+++ index.js
@@ -1,105 +1,105 @@
 
 
-const fs   = require('fs');
+const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
-
-
+  md = new MarkdownIt();
 
 
 const getCommandInPosition = (pos) => {
-	return (process.argv[pos]);
+  return (process.argv[pos]);
 }
 
 const commandToAdd1 = getCommandInPosition(2);
 const commandToAdd2 = getCommandInPosition(3);
-console.log(("Resultado > ")+ (commandToAdd1)+" "+(commandToAdd2));
-
-let directoryPath=[];
-let a=1;
-while(a>0){
-  reviewPath=path.parse(commandToAdd1);
-  console.log(reviewPath.ext);
-  console.log(reviewPath.root);
-  console.log(reviewPath.base);
-  console.log(reviewPath.name);
-
-  console.log(reviewPath.dir);
-
-  path.dirname(commandToAdd1);
-  console.log(path.dirname(commandToAdd1)+"dirname");
 
 
+let directoryPath = [commandToAdd1];
+console.log(directoryPath[0]);
+// let a=1;
+// while(a>0){
+reviewPath = path.parse(commandToAdd1);
+console.log(reviewPath);
+console.log(reviewPath.ext);
+console.log(reviewPath.root);
+console.log(reviewPath.base);
+console.log(reviewPath.name);
 
-fs.readdir(commandToAdd1, (err, data) => {
-  if (err) throw err;
-  console.log(data);
+console.log(reviewPath.dir);
 
-  console.log(path.extname(data[6])+"hola");
+path.dirname(commandToAdd1);
+console.log(path.dirname(commandToAdd1) + " Nombre directorio ");
 
-  for (let i=0; i<data.length; i++){
-  	// if(data[i].substring(data[i].length-2)==="md" ){console.log("hay un archivo md")}else{console.log("no hay")};}
-// console.log(data.withFileTypes());
-//
-//     console.log(data[i].fs.isDirectory());
-    if(path.extname(data[i])!=""){console.log("no es carpeta");
-    if(path.extname(data[i])===".md"){
-    console.log("es archivo md... llamar función")
-    };
-    // console.log(fs.stat());
+// var directoryPath=[commandToAdd1];
 
-     // fs.Dirent(commandToAdd1);
+console.log(directoryPath[0] + " revisando si directory path contiene algo");
+console.log(`${directoryPath[0]} hola`);
 
 
-    }else{console.log("es carpeta... analizando cada archivo encontrado y carpeta")}
-}
+///////////////////////
 
-  });
+function reviewFolders(directoryPath) {
+  // console.log(directoryPath[0] + " primera impresion dentro de la función"); ASÍ NO LO ENTIENDE HAY QUE PASARLO A UNA VARIABLE
+  // console.log(commandToAdd1 + " primera impresion commandToAdd1");
 
-// XQUE CONFUNDE EL DIRECTORIO CON ARCHIVO?
-  // Reads contents of directory `/some/dir`, providing an `Array` of
-  // `fs.Dirent` objects (`entries`)
-  fs.readdir(commandToAdd1, { withFileTypes: true }, (err, entries) => {
-    if (err) throw err;
-    entries.filter((entry) => entry.isDirectory())
-      .forEach((entry) => {
-        console.log(`${entry.name} is a directory`);
-      });
-  });
+let pathSearched=directoryPath[0];
+// console.log(pathSearched+ " pathSearched");
 
-  // const fs = require("fs");
-  //
-  // let path = "/path/to/something";
+  fs.lstat(pathSearched, (err, stats) => {
 
-  fs.lstat('C:/Users/Elizabeth/Documents/javascript/markdown/SCL007-md-links', (err, stats) => {
-
-      if(err){
-          return console.log(err); //Handle error
-}else{
+    if (err) {
+      return console.log(err); //Handle error
+    } else {
       console.log(`Is file: ${stats.isFile()}`);
       console.log(`Is directory: ${stats.isDirectory()}`);
 
-      if(stats.isFile()){console.log("es archivo")} else if (stats.isDirectory()){console.log("es directorio");
+      if (stats.isFile()) { console.log("es archivo") } else if (stats.isDirectory()) {
+        console.log("es directorio");
 
-     directoryPath.push(commandToAdd1);
-     console.log(directoryPath);
+        fs.readdir(pathSearched, (err, data) => {
+          if (err) throw err;
+          console.log(data);
+          for (let i = 0; i < data.length; i++) {
+            let urlPrueba = pathSearched + '/' + data[i];
+            // console.log(urlPrueba);
+            console.log(directoryPath);
+            directoryPath.push(urlPrueba);
 
+          }
 
+          // directoryPath.shift();
+          console.log(directoryPath.length);
+          console.log(directoryPath);
+
+        });
+
+        console.log(directoryPath + " estoy fuera del ciclo readdir");
+
+      }
+      console.log(directoryPath + " estoy fuera del ciclo si es directorio");
     }
-
-
+    console.log(directoryPath + " estoy fuera del ciclo si no es error entonces is es archivo o directorio");
+  });
+  if (directoryPath.length = 0) { return console.log("proceso terminado") };
+  return console.log("ok fin función");
 }
-
-});
 
 // console.log(directoryPath);
 
 
 
-a=0;
-}
+// a=0;
+// }
+
+
+
+
+
+
+
+
+reviewFolders(directoryPath);
 
 
 
@@ -112,38 +112,38 @@ fs.readFile(commandToAdd3, (err, data) => {
   // console.log(data.toString());
 
   var result = md.render(data.toString());
-// para imprimir el contenido del archivo
+  // para imprimir el contenido del archivo
   // console.log(result[46]);
 
-console.log(result.length);
+  console.log(result.length);
 
 
-let linksFound=[];
-let link="";
-let newText=result;
-let path=commandToAdd1+"/"+commandToAdd3;
-let m=-1;
+  let linksFound = [];
+  let link = "";
+  let newText = result;
+  let path = commandToAdd1 + "/" + commandToAdd3;
+  let m = -1;
 
-do{
+  do {
     newText.indexOf("<a href=");
-    m=newText.indexOf("<a href=");
-    let n=newText.indexOf("a>");
-    let extensionString=(n+2)-m;
+    m = newText.indexOf("<a href=");
+    let n = newText.indexOf("a>");
+    let extensionString = (n + 2) - m;
 
-    link=newText.substr(m,extensionString);
+    link = newText.substr(m, extensionString);
 
 
-    let o=link.indexOf(">")+1;
+    let o = link.indexOf(">") + 1;
 
-    let extensionContent=link.length-o-4;
-    let contentLink=link.substr(o,extensionContent);
+    let extensionContent = link.length - o - 4;
+    let contentLink = link.substr(o, extensionContent);
 
-    if(m!=-1){linksFound.push(link,contentLink,path);}
+    if (m != -1) { linksFound.push(link, contentLink, path); }
 
-    newText=newText.substr(n+3);
-} while (m!=-1);
+    newText = newText.substr(n + 3);
+  } while (m != -1);
 
-// console.log(linksFound);
+  // console.log(linksFound);
 
 
 });
@@ -152,16 +152,17 @@ do{
 var fetchUrl = require("fetch").fetchUrl;
 
 // source file is iso-8859-15 but it is converted to utf-8 automatically
-fetchUrl("http://es.wikipedia.org/wiki/Markdown", function(error, meta, body){
-  if(meta.status===200){console.log("vinculo correcto");
-  console.log(meta.status);
-  console.log(meta.responseHeaders);
-  console.log(body);
+fetchUrl("http://es.wikipedia.org/wiki/Markdown", function (error, meta, body) {
+  if (meta.status === 200) {
+    console.log("vinculo correcto");
+    console.log(meta.status);
+    console.log(meta.responseHeaders);
+    console.log(body);
 
-}else{console.log("vinculo incorrecto")}
-    // console.log(meta.status);
-    // console.log(meta.responseHeaders);
-    // console.log(body);
+  } else { console.log("vinculo incorrecto") }
+  // console.log(meta.status);
+  // console.log(meta.responseHeaders);
+  // console.log(body);
 });
 
 // SOLO RECIBE HTTPS
@@ -169,7 +170,7 @@ https.get('https://es.wikipedia.org/wiki/Markdown', (res) => {
   const { statusCode } = res;
   console.log(statusCode);
   const contentType = res.headers['content-type'];
-  console.log("estoy aqui "+contentType);
+  console.log("estoy aqui " + contentType);
 });
 
 
